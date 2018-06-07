1983651f603964a34ff7c8ebf8c017bceb98d4e9
joycewambui <jwambui@protonmail.com>
2018-06-07 18:07:50 +0300

    Add models,have not made migrations yet

--- owner/models.py
+++ owner/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Owner(models.Model):
+   name = models.Charfield(max_length=255)
+   nat_id = models.IntegerField(unique=True,default=number)
+   email = models.EmailField()
+   telephone = models.IntegerField(unique=True)
+   vehicle_num = models.ForeignKey(Vehicle)
+   profile_pic = models.ImageField(upload_to='/media')
+   sacco = models.ForeignKey(Sacco)
+
+
+class Vehicle(models.Model):
+	number_plate = models.Charfield()
+	capacity =  models.IntegerField()
