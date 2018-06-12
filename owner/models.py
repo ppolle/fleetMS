e4453aaf7585ef6d52d3dbefbef6c6d28fc7c45e
Abdullahinur <aabdullahinur@hotmail.com>
2018-06-12 15:17:32 +0300

    nothing to commit

--- owner/models.py
+++ owner/models.py
@@ -1,26 +1,30 @@
-from django.db import models
 from django.conf import settings
-from django.dispatch import receiver
+from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from sacco.models import Sacco
 
 # Create your models here.
-class Owner(models.Model):
-   
-   nat_id = models.IntegerField(unique=True,default=00000000)
-   email = models.EmailField()
-   user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-   telephone = models.IntegerField(unique=True,blank=True)
-   profile_pic = models.ImageField(upload_to='ownerProfile/',blank=True)
-   sacco = models.ForeignKey(Sacco)
 
-   @receiver(post_save, sender = settings.AUTH_USER_MODEL)
-   def update_owner(sender,instance,created,**kwargs):
-   	if created:
-   		Owner.objects.create(user = instance)
-   	instance.owner.save()
+
+class Owner(models.Model):
+
+    nat_id = models.IntegerField(unique=True, default=00000000)
+    email = models.EmailField()
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    telephone = models.IntegerField(unique=True, blank=True)
+    profile_pic = models.ImageField(upload_to='ownerProfile/', blank=True)
+    sacco = models.ForeignKey(Sacco)
+
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def update_owner(sender, instance, created, **kwargs):
+        if created:
+            Owner.objects.create(user=instance)
+        instance.owner.save()
 
 
 class Vehicle(models.Model):
-	number_plate = models.CharField(max_length = 200)
-	capacity =  models.IntegerField()
+    number_plate = models.CharField(max_length=200)
+    capacity = models.IntegerField()
